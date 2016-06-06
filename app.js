5028c9169922410ac59d8f0c3a693e5e9aea564d
caasanchezcu <caasanchezcu@unal.edu.co>
2016-06-06 11:48:27 -0500

    MongoDB connection.

--- app.js
+++ app.js
@@ -5,8 +5,20 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/paymentschedule', function (err)
+{
+  if (err)
+  {
+    console.log('MongoDB connection error', err);
+  }
+  else
+  {
+    console.log('MongoDB connection successful');
+  }
+});
+
+var payments = require('./routes/payments');
 
 var app = express();
 
@@ -22,8 +34,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/payments', payments);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
