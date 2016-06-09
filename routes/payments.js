35c450abdea6984774a7bc7c2b0c0dceab7e325c
caasanchezcu <caasanchezcu@unal.edu.co>
2016-06-09 14:28:06 -0500

    Web service of payments improved.

--- routes/payments.js
+++ routes/payments.js
@@ -5,37 +5,43 @@ var mongoose = require('mongoose');
 var Payment = require('../schemas/Payment.js');
 
 router.get('/', function (req, res, next) {
-    Payment.find(function (err, payments) {
-        if (err) return next(err);
-        res.json(payments);
-    });
+    var query = Payment.find();
+    if (req.query.skip && Number(req.query.skip) >= 0)
+        query.skip(Number(req.query.skip));
+    if (req.query.limit && Number(req.query.limit) >= 0)
+        query.limit(Number(req.query.limit));
+    query.exec(function (err, result) {
+            if (err) return next(err);
+            res.json(result);
+        }
+    );
 });
 
 router.post('/', function (req, res, next) {
-    Payment.create(req.body, function (err, pay) {
+    Payment.create(req.body, function (err, result) {
         if (err) return next(err);
-        res.json(pay)
+        res.json(result)
     })
 });
 
 router.get('/:id', function (req, res, next) {
-    Payment.findById(req.params.id, function (err, pay) {
+    Payment.findById(req.params.id, function (err, result) {
         if (err) return next(err);
-        res.json(pay);
+        res.json(result);
     });
 });
 
 router.put('/:id', function (req, res, next) {
-    Payment.findByIdAndUpdate(req.params.id, req.body, function (err, pay) {
+    Payment.findByIdAndUpdate(req.params.id, req.body, function (err, result) {
         if (err) return next(err);
-        res.json(pay);
+        res.json(result);
     });
 });
 
 router.delete('/:id', function (req, res, next) {
-    Payment.findByIdAndRemove(req.params.id, req.body, function (err, pay) {
+    Payment.findByIdAndRemove(req.params.id, req.body, function (err, result) {
         if (err) return next(err);
-        res.json(pay);
+        res.json(result);
     });
 });
 
--- schemas/Payment.js
+++ schemas/Payment.js
@@ -4,11 +4,12 @@ var PaymentSchema = new mongoose.Schema({
     name: {type: String, required: true},
     date: {type: Date, required: true}, // First time to pay (or the only one if recurrence is 0)
     recurrence: {type: Number, min: 0, required: true}, // In days, 0: only once, 1: every day, 7: one week, 14: two weeks and so on...
-    paymentsDone: [Date] // Recurrences marked as paid.
+    limit: {type: Number, min: 1, required: true}, // Size of paymentsDone, maximum amount of recurrences.
+    paymentsDone: [Boolean] // Paid status of the recurrences.
 });
 
-PaymentSchema.methods.pay = function () {
-    this.payments.push(Date.now());
+PaymentSchema.methods.setPayment = function (index, status) {
+    this.paymentsDone[index] = status;
 };
 
 module.exports = mongoose.model('Payment', PaymentSchema);
