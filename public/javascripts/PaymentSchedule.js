e5c2ac3d89961bf3190c2ec17145c2c1514aa605
caasanchezcu <caasanchezcu@unal.edu.co>
2016-06-09 22:13:32 -0500

    Partial progress in payments frontend.

--- public/javascripts/PaymentSchedule.js
+++ public/javascripts/PaymentSchedule.js
@@ -1,23 +1,68 @@
+"use strict";
 var app = angular.module('PaymentSchedule', ['ngRoute', 'ngMaterial']);
 
 app.controller('NavigationController', ['$scope', '$mdSidenav', function ($scope, $mdSidenav) {
     $scope.toggleSideNav = function () {
         $mdSidenav('sidenav').toggle();
     };
-    $scope.message = 'Hello from HomeController';
+    $scope.infiniteScroll = function () {
+        $scope.$broadcast('infiniteScroll');
+    };
 }]);
 
 app.controller('HomeController', ['$scope', function ($scope) {
-    $scope.message = 'Hello from HomeController';
 }]);
 
 app.controller('CalendarController', ['$scope', function ($scope) {
-    $scope.message = 'Hello from BlogController';
+
 }]);
 
-app.controller('PaymentController', ['$scope', function ($scope) {
-    $scope.message = 'Hello from AboutController';
+app.controller('PaymentController', ['$scope', '$http', function ($scope, $http) {
+    $scope.chunk = 5;
+    $scope.payments = [];
+    $scope.loading = false;
+
+    $scope.loadMore = function () {
+        $scope.loading = true;
+        $http.get('/payments', {
+            params: {skip: $scope.payments.length, limit: $scope.chunk}
+        })
+            .then(function (response) {
+                $scope.payments = $scope.payments.concat(response.data);
+                $scope.loading = false;
+            });
+    };
+    $scope.reload = function () {
+        $scope.loading = true;
+        $http.get('/payments', {
+            params: {skip: 0, limit: $scope.payments.length}
+        })
+            .then(function (response) {
+                $scope.payments = response.data;
+                $scope.loading = false;
+            });
+    };
+
+    $scope.$on('infiniteScroll', function (event) {
+        $scope.loadMore();
+    });
+
+    $scope.loadMore();
 }]);
+
+app.directive('infiniteScroll', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var elm = element[0];
+            element.bind('scroll', function () {
+                if (elm.scrollHeight - elm.scrollTop === elm.clientHeight) {
+                    scope.$apply(attrs.infiniteScroll);
+                }
+            });
+        }
+    }
+});
 
 app.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
     $routeProvider
@@ -29,8 +74,8 @@ app.config(['$locationProvider', '$routeProvider', function ($locationProvider, 
             templateUrl: 'partials/calendar.html',
             controller: 'CalendarController'
         })
-        .when('/payment', {
-            templateUrl: 'partials/payment.html',
+        .when('/payments', {
+            templateUrl: 'partials/payments.html',
             controller: 'PaymentController'
         })
         .otherwise({redirectTo: '/'});
